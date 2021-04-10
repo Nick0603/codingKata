b10c6902667464741504a1357f31551a429aec85
Nick <b10303008@gmail.com>
2021-04-10 19:25:58 +0800

    Card object

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+class Card {
+    constructor(number, suit) {
+        this.number = number;
+        this.suit = suit;
+    }
+    getNumber() {
+        return this.number;
+    }
+    getSuit() {
+        return this.suit;
+    }
+}
+
+module.exports = Card;
--- tests/Card.test.js
+++ tests/Card.test.js
@@ -1,8 +1,25 @@
 const Card = require('../src/Card');
 
 describe('Card', () => {
-    it('should can be new', () => {
+    it('可以被宣告', () => {
         const card = new Card();
         expect(card).toBeInstanceOf(Card);
     })
+    it('是否有這個函式', () => {
+        const card = new Card();
+        expect(card.getNumber).not.toBe(undefined);
+    })
+
+    it('判斷是否可以取到輸入的數字', () => {
+        const card = new Card(1);
+        const number = card.getNumber();
+        expect(number).toBe(1)
+    })
+
+    it('判斷是否可以取到正確的花色', () => {
+        const card = new Card(1,'spade');
+        const suit = card.getSuit();
+        expect(suit).toBe('spade')
+    })
+    
 })
